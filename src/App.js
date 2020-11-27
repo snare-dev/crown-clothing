2022a577225752b4c3a6b52233f477c1afda707a
snare-dev <snare-dev@users.noreply.github.com>
2020-11-27 04:50:19 +0300

    broken hompage into components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
@@ -12,3 +12,4 @@ function App() {
 }
 
 export default App;
+  
